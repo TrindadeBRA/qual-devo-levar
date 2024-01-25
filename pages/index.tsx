df7c1931b8cb29824efeeed847933a26b6d93fd3
Lucas Trindade <trindadebra@gmail.com>
2024-01-24 22:05:07 -0300

    new layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,100 +76,159 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8 p-4 bg-gray-100 text-black rounded-lg shadow-md">
-      <h1 className="text-2xl font-bold mb-6 text-center">Qual Devo Levar?</h1>
-      <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
-        
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-2">Produto 1</label>
-          <div className="flex gap-4">
-            <select
-              value={product1.unit}
-              onChange={(e) => handleUnitChange(e.target.value)}
-              className="p-2 border rounded-md focus:outline-none focus:border-blue-500"
-            >
-              <option value="grama">g</option>
-              <option value="quilo">kg</option>
-              <option value="mililitro">ml</option>
-              <option value="litro">L</option>
-            </select>
-            <input
-              type="number"
-              placeholder="Tamanho/Peso"
-              value={product1.sizeOrWeight}
-              onChange={(e) => setProduct1({ ...product1, sizeOrWeight: parseFloat(e.target.value) })}
-              className="p-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-            <div className="relative">
-              <span className="absolute inset-y-0 left-0 pl-2 flex items-center text-gray-700">
-                R$
-              </span>
-              <input
-                type="number"
-                placeholder="Preço"
-                value={product1.price}
-                onChange={(e) => setProduct1({ ...product1, price: parseFloat(e.target.value) })}
-                className="pl-7 p-2 border rounded-md focus:outline-none focus:border-blue-500"
-              />
+    <div className="p-4 bg-gray-100 text-black rounded-lg shadow-md h-[100vh]">
+
+      <div className="">
+
+        <h1 className="text-2xl font-bold mb-6 mt-4 text-center">Qual devo levar?</h1>
+        <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
+
+          <div className="mb-6">
+            <h2 className="font-bold text-xl">Produto 1</h2>
+            <div>
+              <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+                Tamanho / Dimensões / Peso
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 flex items-center">
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country"
+                    className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 sm:text-sm"
+                    value={product1.unit}
+                    onChange={(e) => handleUnitChange(e.target.value)}
+                  >
+                    <option value="grama">g</option>
+                    <option value="quilo">kg</option>
+                    <option value="mililitro">ml</option>
+                    <option value="litro">L</option>
+                  </select>
+                </div>
+                <input
+                  type="text"
+                  name="phone-number"
+                  id="phone-number"
+                  className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  placeholder=""
+                  value={product1.sizeOrWeight}
+                  onChange={(e) => setProduct1({ ...product1, sizeOrWeight: parseFloat(e.target.value) })}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+                Preço
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <span className="text-gray-500 sm:text-sm">R$</span>
+                </div>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  className="block w-full rounded-md border-0 py-1.5 pl-9 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="0.00"
+                  aria-describedby="price-currency"
+                  value={product1.price}
+                  onChange={(e) => setProduct1({ ...product1, price: parseFloat(e.target.value) })}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-2">Produto 2</label>
-          <div className="flex gap-4">
-            <select
-              value={product2.unit}
-              onChange={(e) => handleUnitChange(e.target.value)}
-              className="p-2 border rounded-md focus:outline-none focus:border-blue-500"
-            >
-              <option value="grama">g</option>
-              <option value="quilo">kg</option>
-              <option value="mililitro">ml</option>
-              <option value="litro">L</option>
-            </select>
-            <input
-              type="number"
-              placeholder="Tamanho/Peso"
-              value={product2.sizeOrWeight}
-              onChange={(e) => setProduct2({ ...product2, sizeOrWeight: parseFloat(e.target.value) })}
-              className="p-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-            <div className="relative">
-              <span className="absolute inset-y-0 left-0 pl-2 flex items-center text-gray-700">
-                R$
-              </span>
-              <input
-                type="number"
-                placeholder="Preço"
-                value={product2.price}
-                onChange={(e) => setProduct2({ ...product2, price: parseFloat(e.target.value) })}
-                className="pl-7 p-2 border rounded-md focus:outline-none focus:border-blue-500"
-              />
+
+          <div>
+            <h2 className="font-bold text-xl">Produto 2</h2>
+            <div>
+              <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+                Tamanho / Dimensões / Peso
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 flex items-center">
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country"
+                    className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 sm:text-sm"
+                    value={product2.unit}
+                    onChange={(e) => handleUnitChange(e.target.value)}
+                  >
+                    <option value="grama">g</option>
+                    <option value="quilo">kg</option>
+                    <option value="mililitro">ml</option>
+                    <option value="litro">L</option>
+                  </select>
+                </div>
+                <input
+                  type="text"
+                  name="phone-number"
+                  id="phone-number"
+                  className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  placeholder=""
+                  value={product2.sizeOrWeight}
+                  onChange={(e) => setProduct2({ ...product2, sizeOrWeight: parseFloat(e.target.value) })}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+                Preço
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <span className="text-gray-500 sm:text-sm">R$</span>
+                </div>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  className="block w-full rounded-md border-0 py-1.5 pl-9 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="0.00"
+                  aria-describedby="price-currency"
+                  value={product2.price}
+                  onChange={(e) => setProduct2({ ...product2, price: parseFloat(e.target.value) })}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <button
-          type="submit"
-          className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
-        >
-          Comparar Preços
-        </button>
-      </form>
 
-      {/* Resultado da comparação */}
-      {comparisonResult && (
-        <div className="mt-6 text-center text-green-600 font-semibold">
-          {comparisonResult}
-        </div>
-      )}
+          <button
+            type="submit"
+            className="mt-3 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+          >
+            Comparar
+          </button>
+        </form>
 
-      {/* Custo por unidade */}
-      {costPerUnit1 !== null && costPerUnit2 !== null && (
-        <div className="mt-4 text-center">
-          <p>{`Custo por unidade de ${product1.name}: R$${costPerUnit1.toFixed(3)}`}</p>
-          <p>{`Custo por unidade de ${product2.name}: R$${costPerUnit2.toFixed(3)}`}</p>
+        <div className="bg-gray-300 p-4 mt-6 rounded-md shadow-md">
+          {/* Resultado da comparação */}
+          {comparisonResult && (
+            <div className="text-center text-green-600 font-semibold">
+              {comparisonResult}
+            </div>
+          )}
+
+          {/* Custo por unidade */}
+          {costPerUnit1 !== null && costPerUnit2 !== null && (
+            <div className="mt-4 text-center">
+              <p>{`Custo por unidade de ${product1.name}: R$${costPerUnit1.toFixed(3)}`}</p>
+              <p>{`Custo por unidade de ${product2.name}: R$${costPerUnit2.toFixed(3)}`}</p>
+            </div>
+          )}
         </div>
-      )}
+
+
+      </div>
+
 
     </div>
   );
